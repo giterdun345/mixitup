815f8e3b9d1ce272c9b9710da57325f0a3831bbf
John Ketterer <johnmketterer@gmail.com>
2021-04-28 19:57:13 -0500

    header begin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
-    <div className="App" class="p-6 items-center justify-center">
-        <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
-        <p class="tracking-widest">This is my first React App.</p>
+    <div>
+        <Header />
     </div>
   );
 }
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCocktail } from '@fortawesome/free-solid-svg-icons'
 const Header = ()=>{
   return(
     <header class="text-gray-600 body-font">
@@ -11,10 +13,13 @@ const Header = ()=>{
           <a class="hover:text-gray-900">Fourth Link</a>
         </nav>
         <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span class="ml-3 text-xl">Tailblocks</span>
+          </svg> */}
+          <span>
+            <FontAwesomeIcon icon={faCocktail} />          
+          </span>
+          <span class="ml-3 text-xl font-logo">MixItUp</span>
         </a>
         <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
           <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
